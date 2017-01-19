593d7c64c285c7b8b7d1cfe6f8cd40a9fc000386
Al West <al@tsew.net>
2017-01-19 20:36:07 +0000

    Added plates-found.txt which shows those plates that can be bought

--- plates-check.sh
+++ plates-check.sh
@@ -4,6 +4,7 @@ while read -r line; do
   curl -s -S http://www.plates4less.co.uk/private-number-plates/${line}/ | tac | grep -q "${found_str}"
   if [ $? -eq 0 ]; then 
     echo -ne "\n${line} Found\n"
+    echo $line >> plates-found.txt
   else
     printf "."
   fi
